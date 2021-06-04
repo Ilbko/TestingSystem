51c6f5c1407999b00fdf37e30a6c2f644e2bacbc
Sepe_4bld@student.itstep.org <Sepe_4bld@student.itstep.org>
2021-06-04 22:59:10 +0300

    Добавьте файлы проекта.

---
+++ WPF_Exam/View/Authentication/AuthWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WPF_Exam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.Background = new SolidColorBrush(Color.FromArgb(255, 51, 51, 51));
+            InitializeComponent();
+
+            string themeName = "AuthTheme";
+
+            var uri = new Uri(@"View/Authentication/" + themeName + ".xaml", UriKind.Relative);
+            ResourceDictionary resourceDictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+    }
+}
